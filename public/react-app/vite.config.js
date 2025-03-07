24152f9d61595ce72901de5519abb3c56887354c
AkosiPaulo <johnpaulo.ramos@cvsu.edu.ph>
2025-03-07 18:02:27 +0800

    react-app/assets has changed

--- public/react-app/vite.config.js
+++ public/react-app/vite.config.js
@@ -4,6 +4,42 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is mainly for the production build to ensure assets are linked correctly
-  base: "/react-app/", // Match the subdirectory where your React app is served
+  
+  base: "/react-app/", 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
